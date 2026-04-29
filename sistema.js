3e89b9338c0937e9069f231bcbead3af64258e0c
Kemily <pontes.kemily@escola.pr.gov.br>
2026-04-29 09:45:34 -0300

    Create sistema.js

---
+++ sistema.js
@@ -0,0 +1,20 @@
+const produtosArquivo = require("./produtos.json")
+console.log(produtosArquivo)
+
+produtosArquivo.forEach((produto)=>{
+    console.log(produto.nome + 
+        " - " + produto.valor + 
+        " -  " + produto.quantidade + 
+        " - " + produto.peso + 
+        " -  " + produto.marca +   
+        " -  " + produto.codigo)
+})
+protudos.push(produto.nome + "bolacha" +
+    " - " + produto.valor + 15 +
+    " -  " + produto.quantidade + 63 +
+    " - " + produto.peso + 1 +
+    " -  " + produto.marca +  "oreo" +
+    " -  " + produto.codigo + 6783)
+    
+
+console.log(produtos)
